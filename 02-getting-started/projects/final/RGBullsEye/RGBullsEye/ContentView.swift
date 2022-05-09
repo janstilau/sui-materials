c45bce3baf9d765d86d14acc35668be2981b5330
JustinLau <janstilau@gmail.com>
2022-05-09 20:23:22 +0800

    sync

--- 02-getting-started/projects/final/RGBullsEye/RGBullsEye/ContentView.swift
+++ 02-getting-started/projects/final/RGBullsEye/RGBullsEye/ContentView.swift
@@ -33,62 +33,62 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State var game = Game()
-  @State var guess: RGB
-  @State var showScore = false
-
-  var body: some View {
-    VStack {
-      Circle()
-        .fill(Color(rgbStruct: game.target))
-      if !showScore {
-        Text("R: ??? G: ??? B: ???")
-          .padding()
-      } else {
-        Text(game.target.intString())
-          .padding()
-      }
-      Circle()
-        .fill(Color(rgbStruct: guess))
-      Text(guess.intString())
-        .padding()
-      ColorSlider(value: $guess.red, trackColor: .red)
-      ColorSlider(value: $guess.green, trackColor: .green)
-      ColorSlider(value: $guess.blue, trackColor: .blue)
-      Button("Hit Me!") {
-        showScore = true
-        game.check(guess: guess)
-      }
-      .alert(isPresented: $showScore) {
-        Alert(
-          title: Text("Your Score"),
-          message: Text(String(game.scoreRound)),
-          dismissButton: .default(Text("OK")) {
-            game.startNewRound()
-            guess = RGB()
-          })
-      }
+    @State var game = Game()
+    @State var guess: RGB
+    @State var showScore = false
+    
+    var body: some View {
+        VStack {
+            Circle()
+                .fill(Color(rgbStruct: game.target))
+            if !showScore {
+                Text("R: ??? G: ??? B: ???")
+                    .padding()
+            } else {
+                Text(game.target.intString())
+                    .padding()
+            }
+            Circle()
+                .fill(Color(rgbStruct: guess))
+            Text(guess.intString())
+                .padding()
+            ColorSlider(value: $guess.red, trackColor: .red)
+            ColorSlider(value: $guess.green, trackColor: .green)
+            ColorSlider(value: $guess.blue, trackColor: .blue)
+            Button("Hit Me!") {
+                showScore = true
+                game.check(guess: guess)
+            }
+            .alert(isPresented: $showScore) {
+                Alert(
+                    title: Text("Your Score"),
+                    message: Text(String(game.scoreRound)),
+                    dismissButton: .default(Text("OK")) {
+                        game.startNewRound()
+                        guess = RGB()
+                    })
+            }
+        }
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView(guess: RGB())
-  }
+    static var previews: some View {
+        ContentView(guess: RGB())
+    }
 }
 
 struct ColorSlider: View {
-  @Binding var value: Double
-  var trackColor: Color
-
-  var body: some View {
-    HStack {
-      Text("0")
-      Slider(value: $value)
-        .accentColor(trackColor)
-      Text("255")
+    @Binding var value: Double
+    var trackColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("0")
+            Slider(value: $value)
+                .accentColor(trackColor)
+            Text("255")
+        }
+        .padding(.horizontal)
     }
-    .padding(.horizontal)
-  }
 }
