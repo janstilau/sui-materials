fab9d71dd0d76aea7c513a58610ac7200283d65c
JustinLau <janstilau@gmail.com>
2022-05-10 15:51:18 +0800

    sync

--- AppleGuide/CreatingAndCombiningViews/Complete/Landmarks/ContentView.swift
+++ AppleGuide/CreatingAndCombiningViews/Complete/Landmarks/ContentView.swift
@@ -1,9 +1,9 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view showing the details for a landmark.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view showing the details for a landmark.
+ */
 
 import SwiftUI
 
@@ -13,15 +13,15 @@ struct ContentView: View {
             MapView()
                 .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
-
+            
             CircleImage()
                 .offset(y: -130)
                 .padding(.bottom, -130)
-
+            
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
-
+                
                 HStack {
                     Text("Joshua Tree National Park")
                     Spacer()
@@ -29,15 +29,15 @@ struct ContentView: View {
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
-
+                
                 Divider()
-
+                
                 Text("About Turtle Rock")
                     .font(.title2)
                 Text("Descriptive text goes here.")
             }
             .padding()
-
+            
             Spacer()
         }
     }
--- AppleGuide/CreatingAndCombiningViews/Complete/Landmarks/MapView.swift
+++ AppleGuide/CreatingAndCombiningViews/Complete/Landmarks/MapView.swift
@@ -1,9 +1,9 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view that presents a map.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view that presents a map.
+ */
 
 import SwiftUI
 import MapKit
@@ -13,7 +13,7 @@ struct MapView: View {
         center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
         span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
     )
-
+    
     var body: some View {
         Map(coordinateRegion: $region)
     }
