43b5a4807da009aa1fe2e62c569b9f7feb7d5cc4
JustinLau <janstilau@gmail.com>
2022-05-28 20:15:35 +0800

    sync

--- Miao/PokeMaster-Finished/PokeMaster/State/DataFlow/Store.swift
+++ Miao/PokeMaster-Finished/PokeMaster/State/DataFlow/Store.swift
@@ -10,23 +10,23 @@ import Combine
 
 class Store: ObservableObject {
     @Published var appState = AppState()
-
+    
     private var disposeBag = Set<AnyCancellable>()
-
+    
     init() {
         setupObservers()
     }
-
+    
     func setupObservers() {
         appState.settings.checker.isValid.sink { isValid in
             self.dispatch(.accountBehaviorButton(enabled: isValid))
         }.store(in: &disposeBag)
-
+        
         appState.settings.checker.isEmailValid.sink { isValid in
             self.dispatch(.emailValid(valid: isValid))
         }.store(in: &disposeBag)
     }
-
+    
     func dispatch(_ action: AppAction) {
         print("[ACTION]: \(action)")
         let result = Store.reduce(state: appState, action: action)
@@ -36,40 +36,40 @@ class Store: ObservableObject {
             command.execute(in: self)
         }
     }
-
+    
     static func reduce(state: AppState, action: AppAction) -> (AppState, AppCommand?) {
         var appState = state
         var appCommand: AppCommand? = nil
-
+        
         switch action {
         case .accountBehaviorButton(let isValid):
             appState.settings.isValid = isValid
-
+            
         case .emailValid(let isValid):
             appState.settings.isEmailValid = isValid
-
+            
         case .register(let email, let password):
             appState.settings.registerRequesting = true
             appCommand = RegisterAppCommand(email: email, password: password)
-
+            
         case .login(let email, let password):
             appState.settings.loginRequesting = true
             appCommand = LoginAppCommand(email: email, password: password)
-
+            
         case .logout:
             appState.settings.loginUser = nil
-
+            
         case .accountBehaviorDone(let result):
             appState.settings.registerRequesting = false
             appState.settings.loginRequesting = false
-
+            
             switch result {
             case .success(let user):
                 appState.settings.loginUser = user
             case .failure(let error):
                 appState.settings.loginError = error
             }
-
+            
         case .toggleListSelection(let index):
             let expanding = appState.pokemonList.selectionState.expandingIndex
             if expanding == index {
@@ -80,19 +80,19 @@ class Store: ObservableObject {
                 appState.pokemonList.selectionState.expandingIndex = index
                 appState.pokemonList.selectionState.panelIndex = index
                 appState.pokemonList.selectionState.radarShouldAnimate =
-                    appState.pokemonList.selectionState.radarProgress == 1 ? false : true
+                appState.pokemonList.selectionState.radarProgress == 1 ? false : true
             }
             
         case .togglePanelPresenting(let presenting):
             appState.pokemonList.selectionState.panelPresented = presenting
             appState.pokemonList.selectionState.radarProgress = presenting ? 1 : 0
-
+            
         case .toggleFavorite(let index):
             guard let loginUser = appState.settings.loginUser else {
                 appState.pokemonList.favoriteError = .requiresLogin
                 break
             }
-
+            
             var newFavorites = loginUser.favoritePokemonIDs
             if newFavorites.contains(index) {
                 newFavorites.remove(index)
@@ -100,14 +100,14 @@ class Store: ObservableObject {
                 newFavorites.insert(index)
             }
             appState.settings.loginUser!.favoritePokemonIDs = newFavorites
-
+            
         case .closeSafariView:
             appState.pokemonList.isSFViewActive = false
-
+            
         case .switchTab(let index):
             appState.pokemonList.selectionState.panelPresented = false
             appState.mainTab.selection = index
-
+            
         case .loadPokemons:
             if appState.pokemonList.loadingPokemons {
                 break
@@ -115,21 +115,21 @@ class Store: ObservableObject {
             appState.pokemonList.pokemonsLoadingError = nil
             appState.pokemonList.loadingPokemons = true
             appCommand = LoadPokemonsCommand()
-
+            
         case .loadPokemonsDone(let result):
             appState.pokemonList.loadingPokemons = false
-
+            
             switch result {
             case .success(let models):
                 appState.pokemonList.pokemons =
-                    Dictionary(uniqueKeysWithValues: models.map { ($0.id, $0) })
+                Dictionary(uniqueKeysWithValues: models.map { ($0.id, $0) })
             case .failure(let error):
                 appState.pokemonList.pokemonsLoadingError = error
             }
-
+            
         case .loadAbilities(let pokemon):
             appCommand = LoadAbilitiesCommand(pokemon: pokemon)
-
+            
         case .loadAbilitiesDone(let result):
             switch result {
             case .success(let loadedAbilities):
@@ -141,7 +141,7 @@ class Store: ObservableObject {
             case .failure(let error):
                 print(error)
             }
-
+            
         case .clearCache:
             appState.pokemonList.pokemons = nil
             appState.pokemonList.abilities = nil
--- Miao/PokeMaster-Finished/PokeMaster/View/Pokemon/Panel/PokemonInfoPanelHeader.swift
+++ Miao/PokeMaster-Finished/PokeMaster/View/Pokemon/Panel/PokemonInfoPanelHeader.swift
@@ -7,7 +7,7 @@
 //
 
 import SwiftUI
-import KingfisherSwiftUI
+
 
 extension PokemonInfoPanel {
     struct Header: View {
@@ -27,7 +27,7 @@ extension PokemonInfoPanel {
         }
 
         var pokemonIcon: some View {
-            KFImage(model.iconImageURL)
+            Image(model.iconImageURL.absoluteString)
                 .resizable()
                 .frame(width: 68, height: 68)
         }
